4892c53b28651646ebaae47af2374a642e783ef2
Anatole Debierre <62328077+a2br@users.noreply.github.com>
2023-08-19 23:42:53 +0200

    add gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ["nuxt-icon", "@pinia/nuxt"],
+	modules: ["nuxt-icon", "@pinia/nuxt", "nuxt-gtag"],
 	ssr: true,
-});
+	gtag: {
+		id: "G-TJ3V7HWRHC",
+	},
+} as any);
