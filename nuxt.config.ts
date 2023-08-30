cd8ec2b27fde3bb45bba9998ae41bd05ee6be5ec
Anatole Debierre <62328077+a2br@users.noreply.github.com>
2023-08-30 12:00:10 +0200

    remove gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,16 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 	modules: ["nuxt-icon", "@pinia/nuxt", "nuxt-gtag"],
 	ssr: true,
-	gtag: {
-		id: "G-TJ3V7HWRHC",
+	app: {
+		head: {
+			script: [
+				{
+					async: true,
+					defer: true,
+					"data-website-id": "1b3df033-91b4-41ee-968a-d0b45e9605c0",
+					src: "https://analytics.sys.epfl.tools/umami.js",
+				},
+			],
+		},
 	},
 } as any);
